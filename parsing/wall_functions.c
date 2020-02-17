a549286b7ef1d5b31a80d2dbe5d96018479eb4f0
Alan LONGCHAMPS <alongcha@e2r3p18.42.fr>
2020-02-17 13:44:00 +0100

    Commence dessin, manquera la fin de build_tree

--- parsing/wall_functions.c
+++ parsing/wall_functions.c
@@ -6,7 +6,7 @@
 /*   By: alongcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 13:16:53 by alongcha          #+#    #+#             */
-/*   Updated: 2020/02/17 13:38:30 by alongcha         ###   ########.fr       */
+/*   Updated: 2020/02/17 13:43:50 by alongcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 /* il est logiquement necessaire de mettre a jour les coordonnees verticales des extr􏰀emites du mur. Mais comme nous avons dit que nous conservions particulierement les coordonnees verticales seulement du debut du mur􏰋les coordonnees verticales des tranches suivantes etant calculees incrementalement grace aux deltas que nous avons calcul􏰀es ci􏰂dessus􏰋 nous n'aurons a nous soucier que de mettre􏰐a jour les coordonnees verticales du debut du mur */
 void	clip(t_wall wall, double *top, double *bot)
 {
-	wall.falseleft.p.x = wall.left.p.x;
+	wall.falseleft.p.x = (wall.left.p.x > 319) 319 : wall.left.p.x;
 	if (wall.left.p.x < 0)
 	{
 		wall.falseleft.p.x = 0;
-		*top -= (double) wall.falseleft.p.x * wall.deltatop;
-		*top -= (double) wall.falseleft.p.x * wall.deltatop;
+		*top -= (double)wall.falseleft.p.x * wall.deltatop;
+		*bot -= (double)wall.falseleft.p.y * wall.deltabot;
 	}
 	wall.falseright.p.x = wall.right.p.x;
 }
