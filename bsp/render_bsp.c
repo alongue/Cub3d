902d1d40cc9b72841790994ff7f9afd0e67710af
Alan LONGCHAMPS <alongcha@e1r10p18.42.fr>
2020-03-10 13:50:59 +0100

    j'essaye de display les wall

--- bsp/render_bsp.c
+++ bsp/render_bsp.c
@@ -6,7 +6,7 @@
 /*   By: alongcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 11:02:24 by alongcha          #+#    #+#             */
-/*   Updated: 2020/03/09 18:17:02 by alongcha         ###   ########.fr       */
+/*   Updated: 2020/03/10 13:50:26 by alongcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,23 @@ void	renderbsp(t_data *data, t_node current, t_player player)
 
 	printf("HOLA !!\n");
 	if (current.isleaf)
+	{
+		display_wall(data, current.splitter.wall);
 		return ;
+	}
 	result = classify_point(current.splitter, player.pos);
 	if (result == FRONT || result == COINCIDING)
 	{
-		if (current.backchild->exist)
+		//if (current.backchild->exist)
 			renderbsp(data, *current.backchild, player);
-		display_wall(data, current.splitter.wall);
-		if (current.frontchild->exist)
+		//if (current.frontchild->exist)
 			renderbsp(data, *current.frontchild, player);
 	}
 	else
 	{
-		if (current.frontchild->exist)
+		//if (current.frontchild->exist)
 			renderbsp(data, *current.frontchild, player);
-		display_wall(data, current.splitter.wall);
-		if (current.backchild->exist)
+		//if (current.backchild->exist)
 			renderbsp(data, *current.backchild, player);
 	}
 }
