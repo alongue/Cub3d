d7131f5f540764036b76040c6143745a97bba19b
Micheal <pmanez381@gmail.com>
2020-05-08 14:21:27 +0200

    conditions en cours

--- bsp/create_poly.c
+++ bsp/create_poly.c
@@ -26,7 +26,7 @@ t_polygon			create_polytop(t_map map, int *coor, t_data data, t_player player)
 	p.normal = get_normal(cub[y][x].stop);
 	p.nbwall = 1;
 	while ((cub[y][++x].exist && cub[y][x - 1].exist
-		&& !cub[y - 1][x].exist && !cub[y - 1][x - 1].exist))
+		&& !cub[y - 1][x].exist && !cub[y - 1][x - 1].exist) && x < get_nbxmax(map.nbcuby) && y < get_nbymax(map.nbcuby))
 	{
 		//printf("p.segment.exist (top) = %d\n", p.segment.exist);
 		p.segment = join_segment(p.segment, cub[y][x].stop);
@@ -74,7 +74,7 @@ t_polygon			create_polybot(t_map map, int *coor, t_data data, t_player player)
 	p.normal = get_normal(cub[y][x].sbot);
 	p.nbwall = 1;
 	while ((cub[y][++x].exist && cub[y][x - 1].exist
-		&& !cub[y + 1][x].exist && !cub[y + 1][x - 1].exist))
+		&& !cub[y + 1][x].exist && !cub[y + 1][x - 1].exist) && x < get_nbxmax(map.nbcuby) && y < get_nbymax(map.nbcuby))
 	{
 		//printf("p.segment.exist (bot boucle) = %d\n", p.segment.exist);
 		p.segment = join_segment(p.segment, cub[y][x].sbot);
@@ -118,7 +118,7 @@ t_polygon			create_polyright(t_map map, int *coor, t_data data, t_player player)
 	p.normal = get_normal(cub[y][x].sright);
 	p.nbwall = 1;
 	while ((cub[++y][x].exist && cub[y - 1][x].exist
-		&& !cub[y - 1][x + 1].exist && !cub[y][x + 1].exist))
+		&& !cub[y - 1][x + 1].exist && !cub[y][x + 1].exist) && x < get_nbxmax(map.nbcuby) && y < get_nbymax(map.nbcuby))
 	{
 		//printf("p.segment.exist (right boucle) = %d\n", p.segment.exist);
 		p.segment = join_segment(p.segment, cub[y][x].sright);
@@ -163,7 +163,7 @@ t_polygon			create_polyleft(t_map map, int *coor, t_data data, t_player player)
 	p.normal = get_normal(cub[y][x].sleft);
 	p.nbwall = 1;
 	while ((cub[++y][x].exist && cub[y - 1][x].exist
-		&& !cub[y - 1][x - 1].exist && !cub[y][x - 1].exist))
+		&& !cub[y - 1][x - 1].exist && !cub[y][x - 1].exist) && x < get_nbxmax(map.nbcuby) && y < get_nbymax(map.nbcuby))
 	{
 		//printf("p.segment.exist (left boucle) = %d\n", p.segment.exist);
 		p.segment = join_segment(p.segment, cub[y][x].sleft);
